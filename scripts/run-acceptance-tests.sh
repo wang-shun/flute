2a4f65fb455776b362eeb2c874a317577be1508b
epickrram <mark@epickrram.com>
2016-10-25 19:21:14 +0100

    put version block in the right place

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ACCEPTANCE_TEST_CLASSES=$(find acceptance/src/main/ -name '*Test.java'|sed -e 's/.java//g'|sed -e 's/\//\./g'|sed -e 's/acceptance.src.main.//g' |tr "\n" " ")
-JAVA_AGENT_ARGS="-javaagent:agent/build/libs/agent-all-0.1.1.jar=$(readlink -f integration/src/test/resources/acceptance-test-flute.properties)"
+JAVA_AGENT_ARGS="-javaagent:agent/build/libs/flute-agent-all-0.1.2.jar=$(readlink -f integration/src/test/resources/acceptance-test-flute.properties)"
 
 
-java $JAVA_AGENT_ARGS -cp acceptance/build/libs/acceptance-all-0.1.1.jar org.junit.runner.JUnitCore $ACCEPTANCE_TEST_CLASSES
+java $JAVA_AGENT_ARGS -cp acceptance/build/libs/flute-acceptance-all-0.1.2.jar org.junit.runner.JUnitCore $ACCEPTANCE_TEST_CLASSES
--- scripts/start-test-client.sh
+++ scripts/start-test-client.sh
@@ -3,4 +3,4 @@ CONFIG_FILE=integration/src/test/resources/acceptance-test-flute.properties
 
 echo "CONFIG_FILE: $CONFIG_FILE"
 
-java -cp integration/build/libs/integration-all-0.1.1.jar com.aitusoftware.flute.integration.client.TimingClientMain $CONFIG_FILE
+java -cp integration/build/libs/flute-integration-all-0.1.2.jar com.aitusoftware.flute.integration.client.TimingClientMain $CONFIG_FILE
