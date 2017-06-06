7474ef084c3d7a4e1af44803fd4287064d6a5675
epickrram <mark@epickrram.com>
2017-06-06 23:02:30 +0100

    bump version

--- scripts/start-test-client.sh
+++ scripts/start-test-client.sh
@@ -3,4 +3,4 @@ CONFIG_FILE=flute-integration/src/test/resources/acceptance-test-flute.propertie
 
 echo "CONFIG_FILE: $CONFIG_FILE"
 
-java -cp flute-integration/build/libs/flute-integration-all-0.1.3.jar com.aitusoftware.flute.integration.client.TimingClientMain $CONFIG_FILE
+java -cp flute-integration/build/libs/flute-integration-all-0.1.4.jar com.aitusoftware.flute.integration.client.TimingClientMain $CONFIG_FILE
