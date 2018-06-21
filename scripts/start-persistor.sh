52c489cd522fa130126fe487ec9412b60b78b576
Mark Price <mark@aitusoftware.com>
2018-06-21 17:35:11 +0100

    Script fixes

--- scripts/start-persistor.sh
+++ scripts/start-persistor.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname $0)"
+SCRIPT_FILE_DIR="$(dirname $0)"
+SCRIPT_DIR="$(readlink -f $SCRIPT_FILE_DIR)"
+LIB_DIR="$(readlink -f $SCRIPT_DIR/../lib)"
 
 CONFIG_FILE="$1"
 if [[ "$CONFIG_FILE" == "" ]]; then
@@ -9,7 +11,7 @@ fi
 
 java -XX:+UnlockCommercialFeatures -XX:+FlightRecorder \
 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 \
--cp $SCRIPT_DIR/lib/flute-persistor-all-FLUTE_VERSION.jar \
+-cp $LIB_DIR/flute-persistor-all-FLUTE_VERSION.jar \
 -Djava.net.preferIPv4Stack=true \
 -Dlog4j.configuration=file://$SCRIPT_DIR/log4j.xml \
 com.aitusoftware.flute.archive.FluteMetricsPersistorMain $CONFIG_FILE
--- scripts/start-report-admin-server.sh
+++ scripts/start-report-admin-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname $0)"
+SCRIPT_FILE_DIR="$(dirname $0)"
+SCRIPT_DIR="$(readlink -f $SCRIPT_FILE_DIR)"
+LIB_DIR="$(readlink -f $SCRIPT_DIR/../lib)"
 
 CONFIG_FILE="$1"
 if [[ "$CONFIG_FILE" == "" ]]; then
@@ -9,6 +11,6 @@ fi
 
 java -XX:+UnlockCommercialFeatures -XX:+FlightRecorder \
 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5007 \
--cp $SCRIPT_DIR/lib/flute-server-all-FLUTE_VERSION.jar  -Djava.net.preferIPv4Stack=true \
+-cp $LIB_DIR/flute-server-all-FLUTE_VERSION.jar  -Djava.net.preferIPv4Stack=true \
 -Dlog4j.configuration=file://$SCRIPT_DIR/log4j.xml \
 com.aitusoftware.flute.server.reporting.http.ReportAdminServerMain $CONFIG_FILE
